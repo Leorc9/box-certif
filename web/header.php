63432493dd8b66196cbe8bcf8e87e717efec00c6
Marc <your-email@example.com>
2026-05-28 12:11:19 +0200

    chore: add auth folder style: add header navigation

--- web/header.php
+++ web/header.php
@@ -1,31 +1,24 @@
 <?php
-session_start();
+// Start the session only if it is not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <ul class="nav">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </li>
-    </ul>
+<link rel="stylesheet" href="/style/headerStyle.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<header class="mainHeader">
+    <nav class="mainNav">
+        <!-- Main navigation links -->
+        <a href="/index.php">Home</a>
+        <a href="#">Trip</a>
+        <a href="#">History</a>
+
+        <!-- If the user is connected, show Log Out, else show Log In -->
+        <?php if (isset($_SESSION["id"])) { ?>
+            <a class="authButton" href="/auth/logOut.php">Log Out</a>
+        <?php } else { ?>
+            <a class="authButton" href="/auth/logIn.php">Log In</a>
+        <?php } ?>
+    </nav>
+</header>
