0617afc992da30cd88ac41e89166eccb9a0e6f0b
Leo <68692328+Leorc9@users.noreply.github.com>
2026-05-28 13:36:01 +0200

    fix: bootstrap reactivation

--- web/header.php
+++ web/header.php
@@ -6,6 +6,8 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 
 <link rel="stylesheet" href="/style/headerStyle.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="/assets/css/style.css" rel="stylesheet">
 
 <header class="mainHeader">
     <nav class="mainNav">
@@ -22,3 +24,7 @@ if (session_status() === PHP_SESSION_NONE) {
         <?php } ?>
     </nav>
 </header>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
